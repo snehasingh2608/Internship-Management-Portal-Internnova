6f6da5ff8b31803e920cfb37cce78cdc8158becb
vartika <vartikav2005@gmail.com>
2026-03-31 06:46:09 +0530

    Added frontend url

--- server/server.js
+++ server/server.js
@@ -35,7 +35,8 @@ app.use(cors({
   origin: [
     "http://localhost:3000",
     "http://localhost:3001",
-    "http://localhost:3002"
+    "http://localhost:3002",
+    "https://internship-management-portal-intern.vercel.app"
   ],
   credentials: true
 }));
